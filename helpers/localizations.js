d89561c57642a47122838a8d804cab7cde789551
Anonymous <>
2024-09-15 00:23:58 +0800

    i18n

--- helpers/localizations.js
+++ helpers/localizations.js
@@ -4,16 +4,16 @@ module.exports = {
     "zh": "👋 请选择您的语言。"
   },
   "helpPrivate": {
-    "en": "😎 *Banofbot* allows you to vote to ban users. Got a spammer or flamer but nobody is out there to ban one? Simply reply to the violator's message with the text `@vote_to_kick_chn_bot`, and the bot will start the voting.\n\n@vote_to_kick_chn_bot works well in group chats — so go on, add it to one of your precious chats! Don't forget to set it as an admin, otherwise it wouldn't work.\n\n/help — Shows this message 😱\n/language — Lets you pick the language 📣",
-    "zh": "😎 *Banofbot* 允许您投票踢出用户。遇到垃圾信息发送者或喷子，但没人能制止他们？只需回复违规者的消息并加上 `@vote_to_kick_chn_bot`，机器人将开始投票。\n\n@vote_to_kick_chn_bot 在群聊中效果很好——快去将它添加到您的重要聊天群里！别忘了将它设为管理员，否则它无法正常工作。\n\n/help — 显示此消息 😱\n/language — 选择语言 📣"
+    "en": "😎 *Banofbot* allows you to vote to ban users. Got a spammer or flamer but nobody is out there to ban one? Simply reply to the violator's message with the text `@vote_to_kick_chn_bot`, and the bot will start the voting.\n\n@vote\\_to\\_kick\\_chn\\_bot works well in group chats — so go on, add it to one of your precious chats! Don't forget to set it as an admin, otherwise it wouldn't work.\n\n/help — Shows this message 😱\n/language — Lets you pick the language 📣",
+    "zh": "😎 *Banofbot* 允许您投票踢出用户。遇到垃圾信息发送者或喷子，但没人能制止他们？只需回复违规者的消息并加上 `@vote_to_kick_chn_bot`，机器人将开始投票。\n\n@vote\\_to\\_kick\\_chn\\_bot 在群聊中效果很好——快去将它添加到您的重要聊天群里！别忘了将它设为管理员，否则它无法正常工作。\n\n/help — 显示此消息 😱\n/language — 选择语言 📣"
   },
   "helpPublic": {
-    "en": "😎 *Banofbot* allows you to vote to ban users. Got a spammer or flamer but nobody is out there to ban one? Simply reply to the violator's message with the text `@vote_to_kick_chn_bot` and the bot will start the voting.\n\n/help — Shows this message 😱\n/language — Lets you pick the language 📣\n/lock — Toggles lock or unlock of non-admins using commands 🔑\n/limit — Lets you set minimal number of voters to kick a user ✌️\n/time — Allows you to select the minimum time between bans\n/votekickWord — allows you to set more votekick words. Use like `/votekickWord kick, trial, challenge` 🐸\n\nDon't forget to set @vote_to_kick_chn_bot as an admin, otherwise it wouldn't work.",
-    "zh": "😎 *Banofbot* 允许您投票踢出用户。遇到垃圾信息发送者或喷子，但没人能制止他们？只需回复违规者的消息并加上 `@vote_to_kick_chn_bot`，机器人将开始投票。\n\n/help — 显示此消息 😱\n/language — 选择语言 📣\n/lock — 切换管理员指令权限 🔑\n/limit — 设置踢出用户的最小投票人数 ✌️\n/time — 设置投票间隔时间\n/votekickWord — 允许设置更多投票词。例如 `/votekickWord 踢出, 审判, 挑战` 🐸\n\n别忘了将 @vote_to_kick_chn_bot 设为管理员，否则它无法正常工作。"
+    "en": "😎 *Banofbot* allows you to vote to ban users. Got a spammer or flamer but nobody is out there to ban one? Simply reply to the violator's message with the text `@vote_to_kick_chn_bot` and the bot will start the voting.\n\n/help — Shows this message 😱\n/language — Lets you pick the language 📣\n/lock — Toggles lock or unlock of non-admins using commands 🔑\n/limit — Lets you set minimal number of voters to kick a user ✌️\n/time — Allows you to select the minimum time between bans\n/votekickWord — allows you to set more votekick words. Use like `/votekickWord kick, trial, challenge` 🐸\n\nDon't forget to set @vote\\_to\\_kick\\_chn\\_bot as an admin, otherwise it wouldn't work.",
+    "zh": "😎 *Banofbot* 允许您投票踢出用户。遇到垃圾信息发送者或喷子，但没人能制止他们？只需回复违规者的消息并加上 `@vote_to_kick_chn_bot`，机器人将开始投票。\n\n/help — 显示此消息 😱\n/language — 选择语言 📣\n/lock — 切换管理员指令权限 🔑\n/limit — 设置踢出用户的最小投票人数 ✌️\n/time — 设置投票间隔时间\n/votekickWord — 允许设置更多投票词。例如 `/votekickWord 踢出, 审判, 挑战` 🐸\n\n别忘了将 @vote\\_to\\_kick\\_chn\\_bot 设为管理员，否则它无法正常工作。"
   },
   "languageSelectedBanofbot": {
-    "en": "@vote_to_kick_chn_bot now speaks English. Thank you!",
-    "zh": "@vote_to_kick_chn_bot 现在使用中文。谢谢！"
+    "en": "@vote\\_to\\_kick\\_chn\\_bot now speaks English. Thank you!",
+    "zh": "@vote\\_to\\_kick\\_chn\\_bot 现在使用中文。谢谢！"
   },
   "lockOnBanofbot": {
     "en": "🔑 Great! *Banofbot* will now respond only to command calls sent by *admins* in this chat.",
@@ -52,16 +52,16 @@ module.exports = {
     "zh": "你已经投了 🔫 的票"
   },
   "adminError": {
-    "en": "🔥 Oops! Looks like @vote_to_kick_chn_bot is not an admin here yet. Please ask admins to set @vote_to_kick_chn_bot as an admin as well, otherwise it will not work. Thanks!",
-    "zh": "🔥 哎呀！看来 @vote_to_kick_chn_bot 还不是管理员。请让管理员将 @vote_to_kick_chn_bot 设为管理员，否则它无法正常工作。谢谢！"
+    "en": "🔥 Oops! Looks like @vote\\_to\\_kick\\_chn\\_bot is not an admin here yet. Please ask admins to set @vote\\_to\\_kick\\_chn\\_bot as an admin as well, otherwise it will not work. Thanks!",
+    "zh": "🔥 哎呀！看来 @vote\\_to\\_kick\\_chn\\_bot 还不是管理员。请让管理员将 @vote\\_to\\_kick\\_chn\\_bot 设为管理员，否则它无法正常工作。谢谢！"
   },
   "limitMessage": {
     "en": "✌️ Please, select the minimum number of votes to kick a user. Current number is *$[1]*",
     "zh": "✌️ 请选择踢出用户的最小投票人数。当前人数为 *$[1]*"
   },
   "limitSuccess": {
-    "en": "@vote_to_kick_chn_bot will now kick a user if *$[1]* people vote for it. Thanks!",
-    "zh": "@vote_to_kick_chn_bot 现在如果有 *$[1]* 人投票踢出该用户，系统将执行踢出操作。谢谢！"
+    "en": "@vote\\_to\\_kick\\_chn\\_bot will now kick a user if *$[1]* people vote for it. Thanks!",
+    "zh": "@vote\\_to\\_kick\\_chn\\_bot 现在如果有 *$[1]* 人投票踢出该用户，系统将执行踢出操作。谢谢！"
   },
   "tooSoonError": {
     "en": "🔥 Looks like you are trying to start a new ban request too soon. You can change the time limit for ban requests by using /time command. Thanks!",
@@ -72,7 +72,7 @@ module.exports = {
     "zh": "✌️ 请选择投票请求之间的最小间隔时间。当前间隔为 *$[1]* 秒。"
   },
   "timeLimitSuccess": {
-    "en": "@vote_to_kick_chn_bot will now allow new ban requests *$[1]* seconds after the last ban. Thanks!",
-    "zh": "@vote_to_kick_chn_bot 现在会在上次踢人之后 *$[1]* 秒允许新的投票请求。谢谢！"
+    "en": "@vote\\_to\\_kick\\_chn\\_bot will now allow new ban requests *$[1]* seconds after the last ban. Thanks!",
+    "zh": "@vote\\_to\\_kick\\_chn\\_bot 现在会在上次踢人之后 *$[1]* 秒允许新的投票请求。谢谢！"
   }
 };
